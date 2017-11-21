8c4f04e05d4e04dd4a47061af47e0861bdc11ad1
mstandif <michaelzs111@gmail.com>
2017-11-21 15:15:01 -0600

    Some clean-up

--- Illuminatrix-Arduino/ino/Illuminum/Illuminum.ino
+++ Illuminatrix-Arduino/ino/Illuminum/Illuminum.ino
@@ -7,6 +7,7 @@ using namespace std;
 #include "Color.cpp"
 #include <FastLED.h>
 
+
 //##################################################################
 //## Main Class ####################################################
 //##################################################################
@@ -14,26 +15,25 @@ using namespace std;
 #define NUM_LEDS 60
 #define DATA_PIN 6
 //#define CLOCK_PIN 5
+#define BRIGHTNESS  64
+#define LED_TYPE    WS2811
+#define COLOR_ORDER GRB
+CRGB leds[NUM_LEDS];
+
+CRGBPalette16 currentPalette;
+TBlendType    currentBlending;
+
 //#define l_R    3    //PWM pin for RED
 //#define l_G    5    //PWM pin for GREEN
 //#define l_B    6    //PWM pin for BLUE
 
 #define BAUD_RATE					9600
-#define NUMBER_OF_SPACES_BEFORE_PWM_IN_SET_CMD		2
-#define NUMBER_OF_SPACES_BEFORE_STATUS_IN_SET_CMD	3
-#define NUMBER_OF_SPACES_BEFORE_LED_NUMBER_IN_SET_CMD	1
-#define DEFAULT_MIN_BRIGHTNESS				500
-#define DEFAULT_MIN_BRIGHTNESS_FOR_SINGLE_COLOR_CYCLE	96
+#define NUMBER_OF_SPACES_BEFORE_RED_IN_SET_CMD    1
+#define NUMBER_OF_SPACES_BEFORE_GREEN_IN_SET_CMD  2
+#define NUMBER_OF_SPACES_BEFORE_BLUE_IN_SET_CMD	  3
+//#define DEFAULT_MIN_BRIGHTNESS				500
+//#define DEFAULT_MIN_BRIGHTNESS_FOR_SINGLE_COLOR_CYCLE	96
 #define DEFAULT_CYCLES_PER_STEP				100
-
-//#define NAME_WHITE	"WHITE"
-//#define	NAME_RED	"RED"
-//#define NAME_GREEN	"GREEN"
-//#define NAME_BLUE	"BLUE"
-//#define	NAME_LIGHTBLUE	"LIGHTBLUE"
-//#define NAME_YELLOW	"YELLOW"
-//#define NAME_PURPLE	"PURPLE"
-//#define NAME_STANDBY	"STANDBY"
 
 
 
@@ -44,16 +44,6 @@ void refreshLEDState(LED led);
 //Globals////
 CRGB leds[NUM_LEDS];
 
-//LED LEDS[3];
-//Color WHITE;
-//Color RED;
-//Color GREEN;
-//Color BLUE;
-//Color LIGHTBLUE;
-//Color YELLOW;
-//Color PURPLE;
-//Color OFF;
-//Color STANDBY;
 String inputString;
 
 //HypnoOrb Components
@@ -65,6 +55,7 @@ int cyclesSinceLastStep;
 int minBrightness;
 int stepsSinceChange;
 int stepsPerHypnoOrbChange;
+
 
 //##################################################################
 //## Init functions ################################################
@@ -84,18 +75,6 @@ void setup() {
   //FastLED.addLeds<APA102>(leds, NUM_LEDS);  //Comment line 15 and this line if using hardware clock and data
   //FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN>(leds, NUM_LEDS); //Uncomment line 16 and this line for software clock 
   //FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, RGB, DATA_RATE_MHZ(12)>(leds, NUM_LEDS); //Uncomment for slower 12MHz option
-  
-	//Create color constants
-	//WHITE.initialize(255, 255, 48);
-	//RED.initialize(255, 0, 0);
-	//GREEN.initialize(0, 255, 0);
-	//BLUE.initialize(0, 0, 255);
-	//LIGHTBLUE.initialize(0, 166, 255);
-	//YELLOW.initialize(255, 255, 0);
-	//PURPLE.initialize(255, 0, 255);
-	//OFF.initialize(0, 0 ,0);
-	//STANDBY.initialize(16, 0, 0);
-
 
 	//Setup for each LED
 	LEDS[0].initialize(NAME_RED, l_R, true, 0, 255, 0);
@@ -130,29 +109,13 @@ void resetColorParameters() {
 	}
 }
 
-
-
 void printLEDs() {
-	for(int i=0;i<3;i++) {
-		Serial.print("LED # ");
-		Serial.print(i);
-		Serial.print(", value: ");
-		Serial.print(LEDS[i].getValue());
-		Serial.print(", state: ");
-		Serial.println(LEDS[i].activated);
-	}
-}
-
-void setLEDs(boolean state){
-	Serial.println("LED state changed");
-	for (int i=0;i<3;i++) {
-		LEDS[i].activated = state;
-	}
-	
-	if (!state) {
-		hypnoOrb = false;
-		resetColorParameters();
-	}
+	Serial.print("LED # ");
+	Serial.print(i);
+	Serial.print(", value: ");
+	Serial.print(LEDS[i].getValue());
+	Serial.print(", state: ");
+	Serial.println(LEDS[i].activated);
 }
 
 char* findSpaceDelimitedSubstring(String input, int numberOfLeadingSpaces) {
@@ -170,7 +133,7 @@ char* findSpaceDelimitedSubstring(String input, int numberOfLeadingSpaces) {
 	}
 
 	//find the end of the substring
-	end=start;
+	end = start;
 	for (int i=start;i<input.length();i++) {
 		if (input.charAt(i) == ' ') {
 			i=input.length();
@@ -186,9 +149,9 @@ char* findSpaceDelimitedSubstring(String input, int numberOfLeadingSpaces) {
 void setLED(String input) {
 	//SET    RED     GRN     BLU
 	//"SET {0-255} {0-255} {0-255}"
-	int red = atoi(findSpaceDelimitedSubstring(input, NUMBER_OF_SPACES_BEFORE_PWM_IN_SET_CMD));
-	int green = atoi(findSpaceDelimitedSubstring(input, NUMBER_OF_SPACES_BEFORE_LED_NUMBER_IN_SET_CMD));
-	int blue = atoi(findSpaceDelimitedSubstring(input, NUMBER_OF_SPACES_BEFORE_STATUS_IN_SET_CMD));
+	int red = atoi(findSpaceDelimitedSubstring(input, NUMBER_OF_SPACES_BEFORE_RED_IN_SET_CMD));
+	int green = atoi(findSpaceDelimitedSubstring(input, NUMBER_OF_SPACES_BEFORE_GREEN_IN_SET_CMD));
+	int blue = atoi(findSpaceDelimitedSubstring(input, NUMBER_OF_SPACES_BEFORE_BLUE_IN_SET_CMD));
   fill_solid( &(leds[i]), NUM_LEDS, CRGB(red, green, blue));
 
 	Serial.print("Changed LED: ");
@@ -199,36 +162,23 @@ void setLED(String input) {
   Serial.print(blue);
 }
 
-void setColor(Color color) {
-	resetColorParameters();	
-
-	for (int i=0;i<3;i++) {
-		LED* led = &LEDS[i];
-		if (led->name.startsWith(NAME_RED)) led->setTarget(color.red);
-		if (led->name.startsWith(NAME_GREEN)) led->setTarget(color.green);
-		if (led->name.startsWith(NAME_BLUE)) led->setTarget(color.blue);
-		led->enable();
-	}
-}
-
 void cycleOn() {
 	resetColorParameters();
 	hypnoOrb=true;
-	setLEDs(true);
 }
 
 void interpretInput(String input) {
-	if (inputString.startsWith("ON")) fill_solid( &(leds[i]), NUM_LEDS, CRGB::Blue); //setColor(BLUE);
-	if (inputString.startsWith("OFF")) fill_solid( &(leds[i]), NUM_LEDS, CRGB::Black);  //setColor(OFF);
+	if (inputString.startsWith("ON")) fill_solid( &(leds[i]), NUM_LEDS, CRGB::Blue); 
+	if (inputString.startsWith("OFF")) fill_solid( &(leds[i]), NUM_LEDS, CRGB::Black);  
 	if (inputString.startsWith("SET")) setLED(inputString);
-	if (inputString.startsWith("WHITE")) fill_solid( &(leds[i]), NUM_LEDS, CRGB::White); //setColor(WHITE);
-	if (inputString.startsWith("RED")) fill_solid( &(leds[i]), NUM_LEDS, CRGB::Red); //setColor(RED);
-	if (inputString.startsWith("GREEN")) fill_solid( &(leds[i]), NUM_LEDS, CRGB::Green);  //setColor(GREEN);
-	if (inputString.startsWith("BLUE")) fill_solid( &(leds[i]), NUM_LEDS, CRGB::Blue);  //setColor(BLUE);
-	if (inputString.startsWith("LIGHTBLUE")) fill_solid( &(leds[i]), NUM_LEDS, CRGB::LightBlue);  //setColor(LIGHTBLUE);
-	if (inputString.startsWith("YELLOW")) fill_solid( &(leds[i]), NUM_LEDS, CRGB::Yellow);  //setColor(YELLOW);
-	if (inputString.startsWith("PURPLE")) fill_solid( &(leds[i]), NUM_LEDS, CRGB::Purple);  //setColor(PURPLE);
-	if (inputString.startsWith("STANDBY")) fill_solid( &(leds[i]), NUM_LEDS, CRGB::Maroon); //setColor(STANDBY);
+	if (inputString.startsWith("WHITE")) fill_solid( &(leds[i]), NUM_LEDS, CRGB::White); 
+	if (inputString.startsWith("RED")) fill_solid( &(leds[i]), NUM_LEDS, CRGB::Red); 
+	if (inputString.startsWith("GREEN")) fill_solid( &(leds[i]), NUM_LEDS, CRGB::Green);  
+	if (inputString.startsWith("BLUE")) fill_solid( &(leds[i]), NUM_LEDS, CRGB::Blue);  
+	if (inputString.startsWith("LIGHTBLUE")) fill_solid( &(leds[i]), NUM_LEDS, CRGB::LightBlue);
+	if (inputString.startsWith("YELLOW")) fill_solid( &(leds[i]), NUM_LEDS, CRGB::Yellow);  
+	if (inputString.startsWith("PURPLE")) fill_solid( &(leds[i]), NUM_LEDS, CRGB::Purple);  
+	if (inputString.startsWith("STANDBY")) fill_solid( &(leds[i]), NUM_LEDS, CRGB::Maroon); 
 	if (inputString.startsWith("CYCLEON")) cycleOn();
 	if (inputString.startsWith("CYCLEOFF")) hypnoOrb=false;
 	if (inputString.startsWith("CYCLEWHITE")) setForWhiteCycle();
